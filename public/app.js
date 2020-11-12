31c57ff03282ad1b3c37cb0fd8fa275f10792d69
dukim <dukim@bnpinnovation.com>
2020-11-12 15:41:00 +0900

    file send 작성중

--- public/app.js
+++ public/app.js
@@ -18,6 +18,28 @@ let remoteStream = null;
 let roomDialog = null;
 let roomId = null;
 
+let sendChannel;
+let receiveChannel;
+let fileReader;
+const bitrateDiv = document.querySelector('div#bitrate');
+const fileInput = document.querySelector('input#fileInput');
+const abortButton = document.querySelector('button#abortButton');
+const downloadAnchor = document.querySelector('a#download');
+const sendProgress = document.querySelector('progress#sendProgress');
+const receiveProgress = document.querySelector('progress#receiveProgress');
+const statusMessage = document.querySelector('span#status');
+const sendFileButton = document.querySelector('button#sendFile');
+
+let receiveBuffer = [];
+let receivedSize = 0;
+
+let bytesPrev = 0;
+let timestampPrev = 0;
+let timestampStart;
+let statsInterval = null;
+let bitrateMax = 0;
+
+
 function init() {
   document.querySelector('#cameraBtn').addEventListener('click', openUserMedia);
   document.querySelector('#hangupBtn').addEventListener('click', hangUp);
@@ -257,6 +279,185 @@ function registerPeerConnectionListeners() {
     console.log(
         `ICE connection state change: ${peerConnection.iceConnectionState}`);
   });
+
+  peerConnection.addEventListener('datachannel', receiveChannelCallback);
 }
 
 init();
+
+// file input change
+fileInput.addEventListener('change', handleFileInputChange, false);
+async function handleFileInputChange() {
+  const file = fileInput.files[0];
+  if (!file) {
+    console.log('No file chosen');
+  } else {
+    sendFileButton.disabled = false;
+  }
+}
+sendFileButton.addEventListener('click', () => createConnection());
+async function createConnection() {
+  abortButton.disabled = false;
+  sendFileButton.disabled = true;
+  
+  sendChannel = peerConnection.createDataChannel('sendDataChannel');
+  sendChannel.binaryType = 'arraybuffer';
+  console.log('Created send data channel');
+
+  sendChannel.addEventListener('open', onSendChannelStateChange);
+  sendChannel.addEventListener('close', onSendChannelStateChange);
+  sendChannel.addEventListener('error', error => console.error('Error in sendChannel:', error));
+
+  fileInput.disabled = true;
+}
+
+function onSendChannelStateChange() {
+  const readyState = sendChannel.readyState;
+  console.log(`Send channel state is: ${readyState}`);
+  if (readyState === 'open') {
+    sendData();
+  }
+}
+
+function sendData() {
+  const file = fileInput.files[0];
+  console.log(`File is ${[file.name, file.size, file.type, file.lastModified].join(' ')}`);
+
+  // Handle 0 size files.
+  statusMessage.textContent = '';
+  downloadAnchor.textContent = '';
+  if (file.size === 0) {
+    bitrateDiv.innerHTML = '';
+    statusMessage.textContent = 'File is empty, please select a non-empty file';
+    closeDataChannels();
+    return;
+  }
+  sendProgress.max = file.size;
+  receiveProgress.max = file.size;
+  const chunkSize = 16384;
+  fileReader = new FileReader();
+  let offset = 0;
+  fileReader.addEventListener('error', error => console.error('Error reading file:', error));
+  fileReader.addEventListener('abort', event => console.log('File reading aborted:', event));
+  fileReader.addEventListener('load', e => {
+    console.log('FileRead.onload ', e);
+    sendChannel.send(e.target.result);
+    offset += e.target.result.byteLength;
+    sendProgress.value = offset;
+    if (offset < file.size) {
+      readSlice(offset);
+    }
+  });
+  const readSlice = o => {
+    console.log('readSlice ', o);
+    const slice = file.slice(offset, o + chunkSize);
+    fileReader.readAsArrayBuffer(slice);
+  };
+  readSlice(0);
+}
+
+function receiveChannelCallback(event) {
+  console.log('Receive Channel Callback');
+  receiveChannel = event.channel;
+  receiveChannel.binaryType = 'arraybuffer';
+  receiveChannel.onmessage = onReceiveMessageCallback;
+  receiveChannel.onopen = onReceiveChannelStateChange;
+  receiveChannel.onclose = onReceiveChannelStateChange;
+
+  receivedSize = 0;
+  bitrateMax = 0;
+  downloadAnchor.textContent = '';
+  downloadAnchor.removeAttribute('download');
+  if (downloadAnchor.href) {
+    URL.revokeObjectURL(downloadAnchor.href);
+    downloadAnchor.removeAttribute('href');
+  }
+}
+
+function onReceiveMessageCallback(event) {
+  console.log(`Received Message ${event.data.byteLength}`);
+  receiveBuffer.push(event.data);
+  receivedSize += event.data.byteLength;
+
+  receiveProgress.value = receivedSize;
+
+  // we are assuming that our signaling protocol told
+  // about the expected file size (and name, hash, etc).
+  const file = fileInput.files[0];
+  if (receivedSize === file.size) {
+    const received = new Blob(receiveBuffer);
+    receiveBuffer = [];
+
+    downloadAnchor.href = URL.createObjectURL(received);
+    downloadAnchor.download = file.name;
+    downloadAnchor.textContent =
+      `Click to download '${file.name}' (${file.size} bytes)`;
+    downloadAnchor.style.display = 'block';
+
+    const bitrate = Math.round(receivedSize * 8 /
+      ((new Date()).getTime() - timestampStart));
+    bitrateDiv.innerHTML =
+      `<strong>Average Bitrate:</strong> ${bitrate} kbits/sec (max: ${bitrateMax} kbits/sec)`;
+
+    if (statsInterval) {
+      clearInterval(statsInterval);
+      statsInterval = null;
+    }
+
+    closeDataChannels();
+  }
+}
+
+async function onReceiveChannelStateChange() {
+  const readyState = receiveChannel.readyState;
+  console.log(`Receive channel state is: ${readyState}`);
+  if (readyState === 'open') {
+    timestampStart = (new Date()).getTime();
+    timestampPrev = timestampStart;
+    statsInterval = setInterval(displayStats, 500);
+    await displayStats();
+  }
+}
+
+// display bitrate statistics.
+async function displayStats() {
+  if (remoteConnection && remoteConnection.iceConnectionState === 'connected') {
+    const stats = await remoteConnection.getStats();
+    let activeCandidatePair;
+    stats.forEach(report => {
+      if (report.type === 'transport') {
+        activeCandidatePair = stats.get(report.selectedCandidatePairId);
+      }
+    });
+    if (activeCandidatePair) {
+      if (timestampPrev === activeCandidatePair.timestamp) {
+        return;
+      }
+      // calculate current bitrate
+      const bytesNow = activeCandidatePair.bytesReceived;
+      const bitrate = Math.round((bytesNow - bytesPrev) * 8 /
+        (activeCandidatePair.timestamp - timestampPrev));
+      bitrateDiv.innerHTML = `<strong>Current Bitrate:</strong> ${bitrate} kbits/sec`;
+      timestampPrev = activeCandidatePair.timestamp;
+      bytesPrev = bytesNow;
+      if (bitrate > bitrateMax) {
+        bitrateMax = bitrate;
+      }
+    }
+  }
+}
+
+function closeDataChannels() {
+  console.log('Closing data channels');
+  sendChannel.close();
+  console.log(`Closed data channel with label: ${sendChannel.label}`);
+  if (receiveChannel) {
+    receiveChannel.close();
+    console.log(`Closed data channel with label: ${receiveChannel.label}`);
+  }
+
+  // re-enable the file select
+  fileInput.disabled = false;
+  abortButton.disabled = true;
+  sendFileButton.disabled = false;
+}
